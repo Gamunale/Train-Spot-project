b8e0b4c50f1560e32cf36740848c81fc9dc5b50e
Karmjeet Nehal <karmnamol@gmail.com>
2022-02-15 11:58:20 -0800

    updated new files

--- server.js
+++ server.js
@@ -1,17 +1,29 @@
-const path = require('path');
 const express = require('express');
-require("dotenv").config();
-
+const app = express();
+const db = require('./models')
+const exphbs = require('express-handlebars');
+const session = require("express-session");
+require('dotenv').config();
 const sequelize = require('./config/connection');
 
-const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
+ 
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
 
-
-
-app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        maxAge: 50000
+    }
+}))
+
+app.use( express.static('public'));
 
 const Account = require("./models/Account.js");
 const Contact = require("./models/Contact.js");
@@ -20,6 +32,12 @@ const Station = require("./models/Station.js");
 const Ticket = require("./models/Ticket.js");
 const Train = require("./models/Train.js");
 
-sequelize.sync({force:true}).then(() => {
-  app.listen(PORT, () => console.log('Listening ' + PORT));
-});
+const authRoutes = require('./controllers/authController');
+app.use("/auth", authRoutes);
+
+const frontEndRoutes = require('./controllers/frontendController');
+app.use(frontEndRoutes);
+
+db.sequelize.sync({force:false}).then(function(){
+    app.listen(PORT);
+})
